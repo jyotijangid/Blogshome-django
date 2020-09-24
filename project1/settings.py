ed8382f03fcd0bd12725ab02895ff9a3bb95f6a4
jyotijangid <39990130+jyotijangid@users.noreply.github.com>
2020-09-24 11:44:12 +0530

    Added django-heroku

--- project1/settings.py
+++ project1/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -140,3 +140,4 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
 '''import socket
 socket.getaddrinfo('localhost', 8080)
 '''
+django_heroku.settings(locals())
