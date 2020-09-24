c4c9581cfcf316ea22f3e3513a3296e5987635f4
jyotijangid <39990130+jyotijangid@users.noreply.github.com>
2020-09-24 16:16:19 +0530

    updated settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -156,4 +156,4 @@ AWS_S3_REGION_NAME = 'us-east-2'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
